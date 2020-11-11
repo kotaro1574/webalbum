b91eca4f105b1fe2ee545a7e7e262e6eaacd1a48
kotaro.sugita <shake1574@gmail.com>
2020-11-11 16:53:14 +0900

    投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   devise_for :users,
     controllers: { registrations: 'registrations'}
     
-  root 'pages#home'
+  root 'posts#index'
 
   get '/users/:id', to: 'users#show', as: 'user'
+
+  resources :posts, only: %i(index new create) do
+    resources :photos, only: %i(create)
+  end
+  
 end
