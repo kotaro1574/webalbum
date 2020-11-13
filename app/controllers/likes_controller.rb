486d89719c5d56c4c673551da08497cdd62d5b7f
kotaro.sugita <shake1574@gmail.com>
2020-11-14 08:42:52 +0900

    コメント機能

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,16 +7,16 @@ class LikesController < ApplicationController
     if @like.save
       respond_to :js
     end
-
-    def destroy
-      @like = Like.find_by(id: params[:id])
-      @post = @like.post
-      if @like.destroy
-        respond_to :js
-      end
+  end
+  
+  def destroy
+    @like = Like.find_by(id: params[:id])
+    @post = @like.post
+    if @like.destroy
+      respond_to :js
     end
   end
-
+  
   private 
     def like_params
       params.permit(:post_id)
